961189e21db412a67472d1c565160eac79ccabc0
Parth_Works <70955588+Parth-Works@users.noreply.github.com>
2023-06-15 19:23:48 +0200

    Updated node_exp.sh

--- node_exp.sh
+++ node_exp.sh
@@ -21,3 +21,18 @@ Description=Node Exporter
 Wants=network-online.target
 After=network-online.target
 
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+echo "" >> /etc/systemd/system/node_exporter.service
+echo "restarting node exporter"
+
+systemctl daemon-reload
+systemctl start node_exporter
